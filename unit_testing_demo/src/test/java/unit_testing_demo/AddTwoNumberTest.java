b70b0f1eb903b1185799519c9d4033f6a32e8cea
Vijaylaxmi <vijaylaxmibambulage7846@gmail.com>
2026-04-08 11:22:23 +0530

    Add files via upload

---
+++ unit_testing_demo/src/test/java/unit_testing_demo/AddTwoNumberTest.java
@@ -0,0 +1,22 @@
+package unit_testing_demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class AddTwoNumberTest {
+	@Test
+    void testAdd() {
+        AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.add(2, 3);
+        assertEquals(5, result, "Addition should return 5");
+    }
+
+    @Test
+    void testSubtract() {
+        AddTwoNumber calc = new AddTwoNumber();
+        int result = calc.subtract(2, 3);
+        assertEquals(-1, result, "Subtraction should return -1");
+    }
+}
+
